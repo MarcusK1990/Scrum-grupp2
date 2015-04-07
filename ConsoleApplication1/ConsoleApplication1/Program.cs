f832828735218338857d93267c6b7d357c570723
CarlAstholm <carl.astholm@gmail.com>
2015-04-07 11:50:23 +0200

    meddelande

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test test test!!!");
+            Console.ReadKey();
         }
     }
 }
