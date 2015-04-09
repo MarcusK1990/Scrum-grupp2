ec66971b6f789cb2843378a77d9fc243bc9ffac7
SofiaEnqvist <sofia_enqvist@hotmail.com>
2015-04-09 11:17:50 +0200

    iegn

---
+++ vIT-System/vIT-System/GUI/frmCompensation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vIT_System.GUI
+{
+    public partial class frmCompensation : Form
+    {
+        public frmCompensation()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+          //  string file = openFileDialog1.FileName;
+            
+        }
+    }
+}
