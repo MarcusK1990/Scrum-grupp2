c8668bc0f44b97665e448844a1cc4fa0662f1d2b
Kmonassar <kimmymonassar@gmail.com>
2015-04-09 09:19:49 +0200

    login 0.2

--- vIT-System/vIT-System/Login.cs
+++ vIT-System/vIT-System/Login.cs
@@ -21,8 +21,28 @@ namespace vIT_System
         {
             string userName = txtUsername.Text;
             string passWord = txtPassword.Text;
-            string sql = "";
+            string sql = "Select password from anstalld where username = '" + userName + "'";
+            //Plats för databaskoppling
 
+            if (!userName.Equals("username"))
+            {
+                MessageBox.Show("Fel användarnamn!");
+                txtUsername.Clear();
+                txtPassword.Clear();
+                return;
+            }
+            if (!passWord.Equals("password"))
+            {
+                MessageBox.Show("Fel lösenord!");
+                txtPassword.Clear();
+                return;
+            }
+            if (userName.Equals("username") && passWord.Equals("password"))
+            {
+                Form1 form = new Form1();
+                form.Visible = true;
+                Visible = false;
+            }
 
         }
 
