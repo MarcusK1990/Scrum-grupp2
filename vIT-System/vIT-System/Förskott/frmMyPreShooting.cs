88ffbdcf3349f8d345c64763e00d084a2c66b26e
annaosterberg88@hotmail.com <annaosterberg88@hotmail..com>
2015-04-22 12:22:41 +0200

    Ändring förskott

--- vIT-System/vIT-System/Förskott/frmMyPreShooting.cs
+++ vIT-System/vIT-System/Förskott/frmMyPreShooting.cs
@@ -37,13 +37,11 @@ namespace vIT_System.Förskott
             var Fid = cbShowFid.SelectedItem.ToString().Substring(0, 1);
             var id = Convert.ToInt32(Fid);
             var Q = sqlHelper.Fetch("select * from Forskott where Forskott.Fid = " + id);
-            var Q1 = sqlHelper.Fetch("select UppId from Forskott where Forskott.Fid = " + id);
-            var Q2 = sqlHelper.Fetch("select * from uppdrag where uppdrag.UppId = "+ Q1);
             if (Q.Rows.Count > 0)
 
             {
                 tbMotivation.Text = Q.Rows[0]["Kommentar"].ToString();
-                tbUppdrag.Text = Q2.Rows[0]["Uppdrag"].ToString();
+                tbUppdrag.Text = Q.Rows[0]["UppId"].ToString();
                 tbSumma.Text = Q.Rows[0]["Summa"].ToString();
                 tbChef.Text = Q.Rows[0]["Chef"].ToString();
                 tbStatus.Text = Q.Rows[0]["Status"].ToString();
